bd28e23f68c7a7d922f5fa215003913e2a490d63
21MountainMan <johnathonwalker@icloud.com>
2015-05-16 17:34:36 -0600

    add an exclamation point

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package com.johnwalker;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello GitHub World");
+        System.out.println("Hello GitHub World!");
     }
 }
